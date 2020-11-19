c0e24b6618374a20ca411857baad7c6bbbf4721f
M.vd.L <milo@dogodigi.net>
2020-11-19 11:28:23 +0100

    Update maintenance.py
    Add status route behind api so database gets created.

--- api/routers/maintenance.py
+++ api/routers/maintenance.py
@@ -11,6 +11,28 @@ def get_db(request: Request):
     return request.state.db
 
 
+@router.get("/api/status/database")
+def get_status_database(db: Session = Depends(get_db)):
+    try:
+        Base.metadata.create_all(bind=engine)
+    except:
+        raise HTTPException(status_code=400, detail="No database connection")
+    finally:
+        db.close()
+    return {"detail": "database connection OK"}
+
+
+@router.get("/api/status/health")
+def get_status_health():
+    return {"detail": "api OK"}
+
+
+@router.get("/api/status/time")
+def get_status_health():
+    current_datetime = datetime.datetime.now()
+    return {"detail": {"time": current_datetime}}
+
+
 @router.get("/status/database")
 def get_status_database(db: Session = Depends(get_db)):
     try:
