73c7ac14a9c6fb2dddbaf9c302775ffaedf68f62
Maarten de Keizer <bitbucket@maartendekeizer.nl>
2020-12-11 22:03:46 +0100

    Disable env validator for cors origins

--- api/settings.py
+++ api/settings.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     BACKEND_CORS_ORIGINS_CSV: List[AnyHttpUrl] = [x.strip() for x in os.environ.get(
         'BACKEND_CORS_ORIGINS_CSV', "http://localhost,http://localhost:4200,http://localhost:3000").split(',')]
 
-    @validator("BACKEND_CORS_ORIGINS_CSV", pre=True)
+    #@validator("BACKEND_CORS_ORIGINS_CSV", pre=True)
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
         if isinstance(v, str) and not v.startswith("["):
             return [i.strip() for i in v.split(",")]
