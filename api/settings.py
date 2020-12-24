aa47e91eb4a4f8145777a21d65aa199f30684ce4
milovanderlinden <milo@dogodigi.net>
2020-12-24 09:54:08 +0100

    Turn off CORS

--- api/settings.py
+++ api/settings.py
@@ -7,16 +7,16 @@ from pydantic import AnyHttpUrl, BaseSettings, validator
 
 class Settings(BaseSettings):
     DATABASE_URL = os.environ.get('DATABASE_URL', "postgresql://postgres:postgres@127.0.0.1:5432/postgres")
-    BACKEND_CORS_ORIGINS_CSV: List[AnyHttpUrl] = [x.strip() for x in os.environ.get(
-        'BACKEND_CORS_ORIGINS_CSV', "http://localhost,http://localhost:4200,http://localhost:3000").split(',')]
+    # BACKEND_CORS_ORIGINS_CSV: List[AnyHttpUrl] = [x.strip() for x in os.environ.get(
+    #     'BACKEND_CORS_ORIGINS_CSV', "http://localhost,http://localhost:4200,http://localhost:3000").split(',')]
 
-    @validator("BACKEND_CORS_ORIGINS_CSV", pre=True)
-    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
-        if isinstance(v, str) and not v.startswith("["):
-            return [i.strip() for i in v.split(",")]
-        elif isinstance(v, (list, str)):
-            return v
-        raise ValueError(v)
+    # @validator("BACKEND_CORS_ORIGINS_CSV", pre=True)
+    # def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+    #     if isinstance(v, str) and not v.startswith("["):
+    #         return [i.strip() for i in v.split(",")]
+    #     elif isinstance(v, (list, str)):
+    #         return v
+    #     raise ValueError(v)
 
 
 settings = Settings()
